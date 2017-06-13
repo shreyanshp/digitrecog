64e8b1983d4aae7bd84ce5e51c0af3af14d3be41
shreyanshp <shreyansh.pandey@fractalanalytics.com>
2017-06-13 15:49:27 +0530

    changed touch and mouse implementation

--- static/index.js
+++ static/index.js
@@ -1,120 +1,104 @@
 (function()
 {
+
+	window.requestAnimFrame = (function (callback) {
+		return window.requestAnimationFrame || 
+					window.webkitRequestAnimationFrame ||
+					window.mozRequestAnimationFrame ||
+					window.oRequestAnimationFrame ||
+					window.msRequestAnimaitonFrame ||
+					function (callback) {
+					 	window.setTimeout(callback, 1000/60);
+					};
+	})();
+
 	var canvas = document.querySelector( "#canvas" );
-	var context = canvas.getContext( "2d" );
 	canvas.width = 280;
 	canvas.height = 280;
 	var ctx;
 	ctx = canvas.getContext('2d');
-	// Variables to keep track of the touch position
-    var touchX,touchY;
-
-    // Draws a dot at a specific position on the supplied canvas name
-    // Parameters are: A canvas context, the x position, the y position, the size of the dot
-    function drawDot(ctx,x,y,size) {
-        // Let's use black by setting RGB values to 0, and 255 alpha (completely opaque)
-        r=0; g=0; b=0; a=255;
-
-        // Select a fill style
-        ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
-
-        // Draw a filled circle
-        ctx.beginPath();
-        ctx.arc(x, y, size, 0, Math.PI*2, true); 
-        ctx.closePath();
-        ctx.fill();
-    } 
-
-
-	var Mouse = { x: 0, y: 0 };
-	var lastMouse = { x: 0, y: 0 };
-	context.fillStyle="white";
-	context.fillRect(0,0,canvas.width,canvas.height);
-	context.color = "black";
-	context.lineWidth = 10;
-    context.lineJoin = context.lineCap = 'round';
+	ctx.strokeStyle = "#222222";
+	ctx.lineWith = 2;
+	ctx.fillStyle="white";
+	ctx.fillRect(0,0,canvas.width,canvas.height);
+	ctx.color = "black";
+	ctx.lineWidth = 8;
+    ctx.lineJoin = ctx.lineCap = 'round';
 	
 	debug();
 
-	canvas.addEventListener( "mousemove", function( e )
-	{
-		lastMouse.x = Mouse.x;
-		lastMouse.y = Mouse.y;
+	var drawing = false;
+	var mousePos = { x:0, y:0 };
+	var lastPos = mousePos;
+	canvas.addEventListener("mousedown", function (e) {
+		drawing = true;
+		lastPos = getMousePos(canvas, e);
+	}, false);
+	canvas.addEventListener("mouseup", function (e) {
+		drawing = false;
+	}, false);
+	canvas.addEventListener("mousemove", function (e) {
+		mousePos = getMousePos(canvas, e);
+	}, false);
 
-		Mouse.x = e.pageX - this.offsetLeft;
-		Mouse.y = e.pageY - this.offsetTop;
+	// Set up touch events for mobile, etc
+	canvas.addEventListener("touchstart", function (e) {
+		mousePos = getTouchPos(canvas, e);
+		var touch = e.touches[0];
+		var mouseEvent = new MouseEvent("mousedown", {
+			clientX: touch.clientX,
+			clientY: touch.clientY
+		});
+		canvas.dispatchEvent(mouseEvent);
+	}, false);
+	canvas.addEventListener("touchend", function (e) {
+		var mouseEvent = new MouseEvent("mouseup", {});
+		canvas.dispatchEvent(mouseEvent);
+		event.preventDefault();
+	}, false);
+	canvas.addEventListener("touchmove", function (e) {
+		var touch = e.touches[0];
+		var mouseEvent = new MouseEvent("mousemove", {
+			clientX: touch.clientX,
+			clientY: touch.clientY
+		});
+		canvas.dispatchEvent(mouseEvent);
+		event.preventDefault();
+	}, false);
 
-	}, false );
+	// Get the position of the mouse relative to the canvas
+	function getMousePos(canvasDom, mouseEvent) {
+		var rect = canvasDom.getBoundingClientRect();
+		return {
+			x: mouseEvent.clientX - rect.left,
+			y: mouseEvent.clientY - rect.top
+		};
+	}
 
-	canvas.addEventListener( "mousedown", function( e )
-	{
-		canvas.addEventListener( "mousemove", onPaint, false );
+	// Get the position of a touch relative to the canvas
+	function getTouchPos(canvasDom, touchEvent) {
+		var rect = canvasDom.getBoundingClientRect();
+		return {
+			x: touchEvent.touches[0].clientX - rect.left,
+			y: touchEvent.touches[0].clientY - rect.top
+		};
+	}
 
-	}, false );
+	// Draw to the canvas
+	function renderCanvas() {
+		if (drawing) {
+			ctx.moveTo(lastPos.x, lastPos.y);
+			ctx.lineTo(mousePos.x, mousePos.y);
+			ctx.stroke();
+			lastPos = mousePos;
+		}
+	}
 
-	canvas.addEventListener( "mouseup", function()
-	{
-		canvas.removeEventListener( "mousemove", onPaint, false );
-
-	}, false );
-
-	var onPaint = function()
-	{	
-		context.lineWidth = context.lineWidth;
-		context.lineJoin = "round";
-		context.lineCap = "round";
-		context.strokeStyle = context.color;
-	
-		context.beginPath();
-		context.moveTo( lastMouse.x, lastMouse.y );
-		context.lineTo( Mouse.x, Mouse.y );
-		context.closePath();
-		context.stroke();
-	};
-
-
-	// Draw something when a touch start is detected
-    function sketchpad_touchStart() {
-        // Update the touch co-ordinates
-        getTouchPos();
-
-        drawDot(ctx,touchX,touchY,12);
-
-        // Prevents an additional mousedown event being triggered
-        event.preventDefault();
-    }
-
-    // Draw something and prevent the default scrolling when touch movement is detected
-    function sketchpad_touchMove(e) { 
-        // Update the touch co-ordinates
-        getTouchPos(e);
-
-        // During a touchmove event, unlike a mousemove event, we don't need to check if the touch is engaged, since there will always be contact with the screen by definition.
-        drawDot(ctx,touchX,touchY,12); 
-
-        // Prevent a scrolling action as a result of this touchmove triggering.
-        event.preventDefault();
-    }
-
-    // Get the touch position relative to the top-left of the canvas
-    // When we get the raw values of pageX and pageY below, they take into account the scrolling on the page
-    // but not the position relative to our target div. We'll adjust them using "target.offsetLeft" and
-    // "target.offsetTop" to get the correct values in relation to the top left of the canvas.
-    function getTouchPos(e) {
-        if (!e)
-            var e = event;
-
-        if(e.touches) {
-            if (e.touches.length == 1) { // Only deal with one finger
-                var touch = e.touches[0]; // Get the information for finger #1
-                touchX=touch.pageX-touch.target.offsetLeft;
-                touchY=touch.pageY-touch.target.offsetTop;
-            }
-        }
-    }
-
-    canvas.addEventListener('touchstart', sketchpad_touchStart, false);
-    canvas.addEventListener('touchmove', sketchpad_touchMove, false);
+	// Allow for animation
+	(function drawLoop () {
+		requestAnimFrame(drawLoop);
+		renderCanvas();
+	})();
 
 	function debug()
 	{
@@ -124,9 +108,17 @@
 		clearButton.on( "click", function()
 		{
 			
-				context.clearRect( 0, 0, 280, 280 );
-				context.fillStyle="white";
-				context.fillRect(0,0,canvas.width,canvas.height);
+			canvas.width = canvas.width;
+			canvas.width = 280;
+			canvas.height = 280;
+			ctx = canvas.getContext('2d');
+			ctx.strokeStyle = "#222222";
+			ctx.lineWith = 2;
+			ctx.fillStyle="white";
+			ctx.fillRect(0,0,canvas.width,canvas.height);
+			ctx.color = "black";
+			ctx.lineWidth = 8;
+		    ctx.lineJoin = ctx.lineCap = 'round';
 			
 		});
 
@@ -135,14 +127,14 @@
 		$( "#colors" ).change(function()
 		{
 			var color = $( "#colors" ).val();
-			context.color = color;
+			ctx.color = color;
 		});
 		
 		/* LINE WIDTH */
 		
 		$( "#lineWidth" ).change(function()
 		{
-			context.lineWidth = $( this ).val();
+			ctx.lineWidth = $( this ).val();
 		});
 	}
 }());
